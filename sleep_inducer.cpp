569c478b03720b4df76a6d3ee18f7d0ed93933a6
parvaraval27 <146870797+parvaraval27@users.noreply.github.com>
2024-04-06 18:31:19 +0530

    Added Insert Function sleep_inducer.cpp
    Added the functionality of inserting the new data.

--- sleep_inducer.cpp
+++ sleep_inducer.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <bits/stdc++.h>
+#include <windows.h>
 using namespace std;
 
 class Inmates
@@ -35,6 +36,81 @@ public:
 };
 
 void insert(Inmates i)
+{
+    bool close = false;
+    while (!close)
+    {
+        system("cls");
+        cout << "\n\n\t\t\t**INSERT\n\n";
+        string name, ID;
+        int dome;
+
+        cout << "\tEnter Name : ";
+        cin >> name;
+        i.setname(name);
+        cout << "\n\n\tEnter ID : ";
+        cin >> ID;
+        i.setID(ID);
+        cout << "\n\n\tEnter Dome no. : ";
+        cin >> dome;
+        i.setdome(dome);
+
+        ofstream out("inmates.txt", ios::app);
+
+        if (!out)
+        {
+            cout << "\tError: File Can't Open!" << endl;
+            return;
+        }
+        else
+        {
+            out << i.getdome() << " : " << i.getID() << " : " << i.getname() << " : ";
+
+            switch (dome)
+            {
+            case 1:
+            {
+                out << "10 : 00" << endl
+                    << endl;
+                break;
+            }
+            case 2:
+            {
+                out << "10 : 30" << endl
+                    << endl;
+                break;
+            }
+            case 3:
+            {
+                out << "11 : 00" << endl
+                    << endl;
+            }
+            }
+        }
+        out.close();
+        system("cls");
+        cout << "Loading";
+        for (int i = 0; i < 5; i++)
+        {
+            cout << ".";
+            Sleep(800);
+        }
+        system("cls");
+        cout << "\n\n\tData Added Successfully";
+        Sleep(2000);
+
+        system("cls");
+        int choice;
+        cout << "\t1.Add more data";
+        cout << "\n\n\t2.close.";
+        cout << "\n\n\tEnter choise: ";
+        cin >> choice;
+        if (choice == 2)
+        {
+            close = true;
+        }
+    }
+}
 
 void search()
 
